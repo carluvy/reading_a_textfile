c9b0791937047a80bfc666e6851c5e2a5e6134bf
Carla Aluvai <aluvaicarla@gmail.com>
2022-05-26 11:44:12 +0300

    initial commit

--- starter-files/Reading-Text-Files/main.py
+++ starter-files/Reading-Text-Files/main.py
@@ -1,16 +1,26 @@
-# Read text from a file, and count the occurence of words in that text
+# Read text from a file, and count the occurrence of words in that text
 # Example:
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
+
 def read_file_content(filename):
-    # [assignment] Add your code here 
-    
-    return "Hello World"
+    # [assignment] Add your code here
+    with open(filename) as file:
+        words = file.readlines()
+
+    return words
 
 
 def count_words():
     text = read_file_content("./story.txt")
-    # [assignment] Add your code here
 
-    return {"as": 10, "would": 20}
+    for sentence in text:
+        word_list = sentence.split()
+        word_dict = {word: len(word) for word in word_list}
+        return word_dict
+
+
+if __name__ == "__main__":
+    wordly = count_words()
+    print(wordly)
